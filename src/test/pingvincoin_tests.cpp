625fdff27ee5c4d7b186db95bc1db2345e35fd58
Jeff Cigrand <kontakt@emperorpenguin.dev>
2022-02-19 10:57:21 +0100

    dogecoin evolves to pingvincoin 3.0 && Fixing copyright.

--- src/test/pingvincoin_tests.cpp
+++ src/test/pingvincoin_tests.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2015 The Pingvincoin Core developers
+// Copyright (c) 2022 The Pingvincoin Core developers
+// Copyright (c) 2015 The Dogecoincoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
