6450b4273d777492945419c28d9f6622ce717a68
Jeff Cigrand <kontakt@emperorpenguin.dev>
2022-02-20 03:15:17 +0100

    does that work?

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -157,7 +157,7 @@ public:
         consensus.hashGenesisBlock = genesis.GetHash();
         digishieldConsensus.hashGenesisBlock = consensus.hashGenesisBlock;
         auxpowConsensus.hashGenesisBlock = consensus.hashGenesisBlock;
-        assert(consensus.hashGenesisBlock == uint256S("0xc3b67565a6258e4af10ad085130176dc7bbd90ff0d6c78ced84abdcc2d48d908"));
+        //assert(consensus.hashGenesisBlock == uint256S("0xc3b67565a6258e4af10ad085130176dc7bbd90ff0d6c78ced84abdcc2d48d908"));
         assert(genesis.hashMerkleRoot == uint256S("0x28a0290adaafabe67a5db1c22036fb9f5f17988bd3691ca4c18f52c0e1b56a2c"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
