c24b44b2db2a42dbb4ca764c486ad7577d4cb21a
Jeff Cigrand <kontakt@emperorpenguin.dev>
2022-02-20 11:31:01 +0100

    let's try to print it

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -159,7 +159,7 @@ public:
         digishieldConsensus.hashGenesisBlock = consensus.hashGenesisBlock;
         auxpowConsensus.hashGenesisBlock = consensus.hashGenesisBlock;
         assert(consensus.hashGenesisBlock == uint256S("0xc3b67565a6258e4af10ad085130176dc7bbd90ff0d6c78ced84abdcc2d48d908"));
-        assert(genesis.hashMerkleRoot == uint256S("0x28a0290adaafabe67a5db1c22036fb9f5f17988bd3691ca4c18f52c0e1b56a2c"));
+        assert(genesis.hashMerkleRoot == uint256S("0x4deff7a000dc7cf771403691ee2abd85a999e8fca38981e77f03ebe903c15323")); //28a0290adaafabe67a5db1c22036fb9f5f17988bd3691ca4c18f52c0e1b56a2c
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         //vSeeds.push_back(CDNSSeedData("multiping.org", "seed.multiping.org", true));
